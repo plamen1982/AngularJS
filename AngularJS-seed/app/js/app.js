88a9c434143a08b077a8e4c854c62b7c41110615
plamen1982 <plamen.hristov@ymail.com>
2017-03-19 12:31:48 -0400

    First Commit- AngularJS-Seed + Video System Project

---
+++ AngularJS-seed/app/js/app.js
@@ -0,0 +1,1 @@
+var peshoApp = angular.module('peshoApp', [])
---
+++ AngularJS-seed/app/js/controllers.js
@@ -0,0 +1,9 @@
+peshoApp.controller('PeshoController', function PeshoController($scope) {
+  $scope.pesho = {
+        name: "Pesho",
+        photo: "https://s-media-cache-ak0.pinimg.com/originals/52/f2/04/52f2046847cfd51df023770d340913c8.jpg",
+        grade: 5,
+        school: "High School of Mathematics",
+        teacher: "Gichka Pesheva"
+    }
+})
