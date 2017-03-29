97e354dd5d9f8ad0d88cd7dbc72ea20d18328123
plamen1982 <plamen.hristov@ymail.com>
2017-03-28 20:51:12 -0400

    Fix bugs to service and to the  html

--- VideoSystem/controllers/videoSystemCtrl.js
+++ VideoSystem/controllers/videoSystemCtrl.js
@@ -1,18 +1,8 @@
-MACodingClubVideoSystem.controller('VideoSystemCtrl', ['$scope', 'videoService', function($scope, videoService){
+MACodingClubVideoSystem.controller('VideoSystemCtrl', ['$scope', 'videoService', function VideoSystemCtrl ($scope, videoService){
     // $scope.videoObject = videoService[0].videoObject;
-    $scope.videoObjectArray = [];
-    console.log(videoService[0])
-    if(videoService[0]){
-        $scope.videoObjectArray.push(videoService[0].videoObject);
-        console.log($scope.videoObjectArray[0].videoObject)
-    } else (
-        console.log('Something does not work!')
-    )
 
-    $scope.addVideoObject = function addVideoObject(videoObject){
+    $scope.videoObjectArray = videoService.getAllVideos();                    
+        $scope.addVideoObject = function addVideoObject(videoObject){
         $scope.videoObjectArray.push(videoObject)
     }
-    
-    $scope.comments = $scope.videoObjectArray[0].comments;
-    
 }])
--- VideoSystem/services/videoService.js
+++ VideoSystem/services/videoService.js
@@ -1,14 +1,17 @@
 MACodingClubVideoSystem.factory('videoService', function(){
-    return [{
-    videoObject: {
-    title: 'Course introduction',
-    pictureUrl: 'http://lorempixel.com/400/200/',
-    length: '3:32',
-    category: 'IT',
-    subscribers: 3,
-    date: new Date(2014, 12, 15).toDateString(),
-    haveSubtitles: false,
-    comments: [
+    return {
+        getAllVideos: function(){
+    
+        return [{
+        videoObject: {
+        title: 'Course introduction video',
+        pictureUrl: 'http://lorempixel.com/400/200/',
+        length: '3:32',
+        category: 'IT',
+        subscribers: 3,
+        date: new Date(2014, 12, 15).toDateString(),
+        haveSubtitles: false,
+        comments: [
     {
         username: 'Pesho Peshev',
         content: 'Congratulations Peshko!',
@@ -19,20 +22,56 @@ MACodingClubVideoSystem.factory('videoService', function(){
     {
         username: 'pax',
         content: 'Congratulations paxi!',
-        date: new Date(2014, 12, 15, 12, 30, 0).toDateString(),
+        date: new Date(2013, 12, 15, 12, 30, 0).toDateString(),
         likes: 322,
-        websiteUrl: 'http://pesho.com/'
+        websiteUrl: 'http://macodingclub.com'
     },
     {
         username: 'Misho  Mishkov',
         content: 'Congratulations Mishka!',
-        date: new Date(2014, 12, 15, 12, 30, 0).toDateString(),
+        date: new Date(2012, 12, 15, 12, 30, 0).toDateString(),
         likes: 32,
-        websiteUrl: 'http://pesho.com/'
+        websiteUrl: 'http://misho.com/'
     }
-
                 ]
             }
-        }]
+        },
+        {
+        videoObject: {
+        title: 'Auto rock video',
+        pictureUrl: 'http://lorempixel.com/400/200/',
+        length: '3:32',
+        category: 'Auto',
+        subscribers: 4,
+        date: new Date(2014, 12, 15).toDateString(),
+        haveSubtitles: false,
+        comments: [
+    {
+        username: 'Tasho Tashev',
+        content: 'Congratulations Tashko!',
+        date: new Date(2013, 12, 15, 12, 30, 0).toDateString(),
+        likes: 1,
+        websiteUrl: 'http://tashistiq.com/'
+    },
+    {
+        username: 'max',
+        content: 'Congratulations maxi!',
+        date: new Date(2012, 12, 15, 12, 30, 0).toDateString(),
+        likes: 112,
+        websiteUrl: 'http://maxi.com/'
+    },
+    {
+        username: 'Pisho  Pishkov',
+        content: 'Congratulations Pishka!',
+        date: new Date(2011, 12, 15, 12, 30, 0).toDateString(),
+        likes: 12,
+        websiteUrl: 'http://pishko.com/'
+    }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
     }
 )
